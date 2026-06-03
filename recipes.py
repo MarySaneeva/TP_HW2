403aab82a6ef9e83ede12117c05f3fa865c2fb51
MarySaneeva <_@example.com>
2026-06-04 02:02:59 +0300

    Реализован класс Recipe

--- recipes.py
+++ recipes.py
@@ -25,3 +25,32 @@ class Ingredient:
             return self.name == other.name and self.unit == other.unit
         return False
 
+class Recipe:
+    def __init__(self, title, ingredients):
+        self.title = title
+        self.ingredients = ingredients
+
+    def add_ingredient(self, ingredient):
+        for i in self.ingredients:
+            if i == ingredient:
+                i.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and not isinstance(ratio, bool) and ratio > 0
+
+    def scale(self, ratio):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Коэффициент масштабирования должен быть положительным числом")
+
+        scale_ingredients = [Ingredient(ing.name, ing.quantity * ratio, ing.unit) for ing in self.ingredients]
+        return Recipe(self.title, scale_ingredients)
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        ingredients_str = "\n".join([f" - {ing}" for ing in self.ingredients])
+        return f"Рецепт: {self.title}\nИнгридиенты:\n{ingredients_str}"
